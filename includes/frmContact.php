8532d0b0f09ce99e052e285321fdbfc66b77ad96
Frédéric NOËL <contact@fredericnoel.com>
2019-12-06 15:23:08 +0100

    Formulaire de contact (HTML)

---
+++ includes/frmContact.php
@@ -0,0 +1,18 @@
+<form action="index.php?page=contact" method="post">
+  <div>
+    <label for="nom">Nom :</label>
+    <input type="text" name="nom" id="nom" />
+  </div>
+  <div>
+    <label for="prenom">Prénom :</label>
+    <input type="text" name="prenom" id="prenom" />
+  </div>
+  <div>
+    <label for="mail">e-mail :</label>
+    <input type="email" name="mail" id="mail" />
+  </div>
+  <div>
+    <label for="msg">Message :</label>
+    <textarea name="msg" id="msg"></textarea>
+  </div>
+</form>
