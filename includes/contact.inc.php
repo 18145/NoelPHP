3dbb97dcfaf6af3b168639d39e5042766b7eca36
Frédéric NOËL <contact@fredericnoel.com>
2019-12-11 12:27:08 +0100

    Test erreurs et stockage pour affichage

--- includes/contact.inc.php
+++ includes/contact.inc.php
@@ -1,11 +1,47 @@
 <h1>Pour nous contacter</h1>
 <?php
 if (isset($_POST['frmContact'])) {
-  echo "Je viens du formulaire";
+  $nom = checkInput($_POST['nom']);
+  $prenom = checkInput($_POST['prenom']);
+  $mail = checkInput($_POST['mail']);
+  $msg = checkInput($_POST['msg']);
+
+  $erreur = array();
+
+  if ($nom === "")
+    array_push($erreur, "Veuillez saisir votre nom");
+
+  if ($prenom === "")
+    array_push($erreur, "Veuillez saisir un prénom");
+
+  if ($mail === "")
+    array_push($erreur, "Veuillez saisir une adresse mail");
+
+  if ($msg === "")
+    array_push($erreur, "Veuillez saisir un message");
+
+  if (count($erreur) > 0) {
+    $message = '<ul>';
+
+    for($i = 0 ; $i < count($erreur) ; $i++) {
+      $message .= '<li>';
+      $message .= $erreur[$i];
+      $message .= '</li>';
+    }
+
+    $message .= '</ul>';
+
+    echo $message;
+
+    require 'frmContact.php';
+  }
+
+  else {
+    echo "Insertion en BDD";
+  }
+
 }
 
 else {
-  echo "Je viens du futur";
+  require 'frmContact.php';
 }
-
-require 'frmContact.php';
