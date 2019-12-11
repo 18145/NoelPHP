9aeed79b0f6217213944345df627828bc7ad8342
Frédéric NOËL <contact@fredericnoel.com>
2019-12-11 16:36:36 +0100

    Exécution requête insertion

--- includes/contact.inc.php
+++ includes/contact.inc.php
@@ -38,10 +38,18 @@ if (isset($_POST['frmContact'])) {
 
   else {
     $sql = "INSERT INTO clients
-  (nom, prenom, mail, message)
-  VALUES ('" . $nom . "', '" . $prenom . "', '" . $mail ."', '" . $msg ."')";
+    (nom, prenom, mail, message)
+    VALUES ('" . $nom . "', '" . $prenom . "', '" . $mail ."', '" . $msg ."')";
 
+    $query = $pdo->prepare($sql);
+    $query->bindValue('nom', $nom, PDO::PARAM_STR);
+    $query->bindValue('prenom', $prenom, PDO::PARAM_STR);
+    $query->bindValue('mail', $mail, PDO::PARAM_STR);
+    $query->bindValue('message', $msg, PDO::PARAM_STR);
 
+    $query->execute();
+
+    echo "ENregistrement OK";
   }
 }
 
